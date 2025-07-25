c4209e4f2123d9967bf20cee44981fe452549514
Andy Lisowski <alisowski@sellars.com>
2025-07-24 23:52:37 -0500

    Small changes

--- InvoiceUpload/__init__.py
+++ InvoiceUpload/__init__.py
@@ -1,7 +1,9 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from shared import utils
+
 import logging
 import tempfile
 import pdfplumber
